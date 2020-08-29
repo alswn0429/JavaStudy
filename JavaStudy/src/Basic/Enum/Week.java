1bbd865f03d596fae9bd9b3547e63000b102d702
mingming <mingming@218.38.137.28>
2020-08-30 03:15:42 +0900

    enum파일을 이용해 열거타입 소스파일 생성
    월화수목금토일!

---
+++ JavaStudy/src/Basic/Enum/Week.java
@@ -0,0 +1,15 @@
+package Basic.Enum;
+
+/*
+ * 열거타입을 선언하기 위해서는 먼저 열거 타입 이름으로 소스파일(.java)를 생성해야함
+ * 관례적으로 모두 대문자로 쓴다!
+ */
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
